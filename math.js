52542be70684982cd6906ecb7b56823a419fb90a
Amandeep Singh <amandeepronaldo@gmail.com>
2026-01-28 23:01:26 +0530

    learning node js

---
+++ math.js
@@ -0,0 +1,10 @@
+// here we are understanding module exports and imports in Node.js
+// refer to hello.js for how these functions are imported and used
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+
+module.exports = { add, subtract };
